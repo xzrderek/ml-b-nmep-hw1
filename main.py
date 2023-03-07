aacdda4a60998ce9723b08c63b298ce129bfda8f
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-06 16:04:11 -0800

    open json of info

--- main.py
+++ main.py
@@ -221,6 +221,20 @@ if __name__ == "__main__":
     args, config = parse_option()
     logger = create_logger(output_dir=config.OUTPUT, name=f"{config.MODEL.NAME}")
     print(json.dumps(vars(args)))
+    # Opening JSON file
+    f = open(vars(args))
+
+    # returns JSON object as 
+    # a dictionary
+    data = json.load(f)
+
+    # Iterating through the json
+    # list
+    for i in data['emp_details']:
+        print(i)
+
+    # Closing file
+    f.close()
 
     seed = config.SEED
     torch.manual_seed(seed)
