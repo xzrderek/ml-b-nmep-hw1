cffbc26c0a5c4279f5d41c13ea4cbc56ddcf681e
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-11 16:59:13 -0800

    alexnet test

--- models/alexnet.py
+++ models/alexnet.py
@@ -6,26 +6,41 @@ class AlexNet(nn.Module):
 
     def __init__(self, num_classes: int = 200) -> None:
         super(AlexNet, self).__init__()
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 96, kernel_size=11, stride=4, padding=0),
-            nn.BatchNorm2d(96),
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            #nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2))
-       
-        self.lin1 = nn.Sequential(
+        
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            #nn.BatchNorm2d(96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            
+            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            nn.AdaptiveAvgPool2d((6, 6))   
+   
+        self.lin = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(9216, 4096),
             nn.ReLU())
-        self.lin2 = nn.Sequential(
+    
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
             nn.ReLU())
-        self.lin3 = nn.Sequential(
+            
             nn.Linear(4096, num_classes))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.conv1(x)
-        out = self.lin1(out)
-        out = self.lin2(out)
-        out = self.lin3(out)
+        out = self.conv(x)
+        out = self.lin(out)
         return out
