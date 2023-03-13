3b13f535b271481ac267fded0147014efe3a3731
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-12 18:11:40 -0700

    sorry more syntax = sms

--- models/alexnet.py
+++ models/alexnet.py
@@ -11,21 +11,16 @@ class AlexNet(nn.Module):
             #nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2)),
-        
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             #nn.BatchNorm2d(96),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size = 3, stride = 2),
-            
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(),
-            
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(),
-            
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(),
-            
             nn.MaxPool2d(kernel_size = 3, stride = 2),
             nn.AdaptiveAvgPool2d((6, 6))   
    
@@ -33,11 +28,9 @@ class AlexNet(nn.Module):
             nn.Dropout(0.5),
             nn.Linear(9216, 4096),
             nn.ReLU(),
-    
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
             nn.ReLU(),
-            
             nn.Linear(4096, num_classes))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
