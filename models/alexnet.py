dfe1b880520befc35e0852aaa49fcd46a868444b
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-13 16:34:03 -0700

    test switch to Lenet

--- models/alexnet.py
+++ models/alexnet.py
@@ -5,6 +5,33 @@ class AlexNet(nn.Module):
     """Fake LeNet with 32x32 color images and 200 classes"""
 
     def __init__(self, num_classes: int = 200) -> None:
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 6, kernel_size=5, stride=1, padding=2),
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0),
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(16 * 6 * 6, 120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.Sigmoid(),
+            nn.Linear(84, num_classes),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+class AlexNet2(nn.Module):
+    """Fake LeNet with 32x32 color images and 200 classes"""
+
+    def __init__(self, num_classes: int = 200) -> None:
         super(AlexNet, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
