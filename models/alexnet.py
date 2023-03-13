1d015a3634b5c0300579585da903c46c74103e30
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-12 18:07:51 -0700

    syntax

--- models/alexnet.py
+++ models/alexnet.py
@@ -10,12 +10,12 @@ class AlexNet(nn.Module):
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             #nn.BatchNorm2d(96),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            nn.MaxPool2d(kernel_size = 3, stride = 2)),
         
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
             #nn.BatchNorm2d(96),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            nn.MaxPool2d(kernel_size = 3, stride = 2)),
             
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
             nn.ReLU(),
@@ -26,17 +26,17 @@ class AlexNet(nn.Module):
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
             nn.ReLU(),
             
-            nn.MaxPool2d(kernel_size = 3, stride = 2))
+            nn.MaxPool2d(kernel_size = 3, stride = 2)),
             nn.AdaptiveAvgPool2d((6, 6))   
    
         self.lin = nn.Sequential(
             nn.Dropout(0.5),
             nn.Linear(9216, 4096),
-            nn.ReLU())
+            nn.ReLU(),
     
             nn.Dropout(0.5),
             nn.Linear(4096, 4096),
-            nn.ReLU())
+            nn.ReLU(),
             
             nn.Linear(4096, num_classes))
 
