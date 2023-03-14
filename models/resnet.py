d7b0914a83fcf1b31932a3de2aa7823b3bb3bf3e
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-14 13:52:59 -0700

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -34,17 +34,17 @@ class ResNetBlock(nn.Module):
     When performing a forward pass, the ReLU activation should be applied after the first batchnorm layer
     and after the second batchnorm gets added to the shortcut.
     """
-    def __init__(self,in_channels, out_channels, stride = 1, identity_downsample = None):
+    def __init__(self, in_channels, out_channels, stride = 1):
         super(ResNetBlock, self).__init__()
         
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
         )
         
         self.conv2 = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_channels),
             nn.ReLU()
         )
