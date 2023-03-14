12ee094521001f3c5ea113dabc7f185cf95ccff9
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-13 21:14:51 -0700

    sms

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def main(config):
     # start a new wandb run to track this script
     wandb.init(
         # set the wandb project where this run will be logged
-        project="my-awesome-project",
+        project="nmep graph",
 
         # track hyperparameters and run metadata
         config={
