4925841a7a563ec075adbf7661e5679885923f1f
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-14 00:43:45 -0700

    sms

--- models/resnet.py
+++ models/resnet.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 class ResNetBlock(nn.Module):
     expansion: int = 1
     def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
-        super(ResidualBlock, self).__init__()
+        super(ResNetBlock, self).__init__()
         self.conv1 = nn.Sequential(
                         nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
                         nn.BatchNorm2d(out_channels),
