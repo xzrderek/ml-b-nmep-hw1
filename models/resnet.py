a9852d9aa7f9709235e4611227ac470d5ecbb746
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-14 13:38:23 -0700

    test resnet comments

--- models/resnet.py
+++ models/resnet.py
@@ -36,37 +36,32 @@ class ResNetBlock(nn.Module):
     """
     def __init__(self,in_channels, out_channels, stride = 1, identity_downsample = None):
         super(ResNetBlock, self).__init__()
-        self.num_layers = 18
-        self.expansion = 1
-        # ResNet50, 101, and 152 include additional layer of 1x1 kernels
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         
         self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
-        
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
-        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
-        self.relu = nn.ReLU()
-        self.identity_downsample = identity_downsample
+        self.shortcut = nn.Sequential()
+        if in_channels != out_channels or stride != 1:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=1)
+                nn.BatchNorm2d(out_channels)  
+            )
+                    
+        
 
     def forward(self, x):
-        identity = x
-        if self.num_layers > 34:
-            x = self.conv1(x)
-            x = self.bn1(x)
-            x = self.relu(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        
         x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
-        x = self.conv3(x)
-        x = self.bn3(x)
-
-        if self.identity_downsample is not None:
-            identity = self.identity_downsample(identity)
-
-        x += identity
-        x = self.relu(x)
+        
+        x = self.shorcut(x)
+        
         return x
 
 class ResNet18(nn.Module):
