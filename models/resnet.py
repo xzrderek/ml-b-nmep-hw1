cae785fba954dda913917a07bc866d3288a2080d
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-14 00:36:13 -0700

    test2.2

--- models/resnet.py
+++ models/resnet.py
@@ -16,16 +16,13 @@ class ResNetBlock(nn.Module):
         super().__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
-        self.conv1 = conv3x3(in_channels, out_channels, stride)
+        self.conv1 = nn.Conv2d(in_channels,out_channels,kernel_size=3,stride=stride,padding=1)
         self.bn1 = norm_layer(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(out_channels, out_channels)
+        self.conv2 = nn.Conv2d(out_channels,out_channels,kernel_size=3,stride=stride,padding=1)
         self.bn2 = norm_layer(out_channels)
         self.stride = stride
         self.shortcut = nn.Sequential()
-
-    def conv3x3(in_channels, out_channels, stride = 1):
-      return nn.Conv2d(in_channels,out_channels,kernel_size=3,stride=stride,padding=1)
 
     def forward(self, x):
         identity = x
