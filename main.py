32aae76622e6bdacd59691a14fe296716b8f011d
natehaynam <114325192+natehaynam@users.noreply.github.com>
2023-03-15 00:47:48 -0700

    transforms graph

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def main(config):
     # start a new wandb run to track this script
     wandb.init(
         # set the wandb project where this run will be logged
-        project="nmep resnet graph",
+        project="nmep transforms graph",
 
         # track hyperparameters and run metadata
         config={
